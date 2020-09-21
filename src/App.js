6b77face32374d841e9498742141faff9939b5bf
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2020-09-21 20:57:11 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar.js";
+import "./App.css";
 
+function App(){
+  return(
+    <div className="App">
+      <Navbar />
+    
+    </div>
+  
+  );
+
+
+}
+
+export default App;
