22b3b47d1be9678aef215a1641e4bcd08b4bca08
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2020-09-22 14:40:27 +0800

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,1 +1,45 @@
+import React, {Component} from "react";
+import {MenuItems} from "./MenuItems";
+import {Button} from "../Button";
+import "./Navbar.css";
+
+Class Navbar extends Component{
+  
+  state = { clicked : false};
+  
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked});
+    
+  render(){
+    return(
+      <nav className="NavItems">
+      <h1 className="NavbarLogo">T-Shirt Seller<i className="fas fa-tshirt"></i></h1>
+      <div className="menu-icon" onClick={this.handleClick}>
+       <i className= {this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+      
+      </div>
+      <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+      {MenuItems.map((item, index) => {
+       return(
+         <li key={index}>
+          <a className = {item.cName} href={url}>
+          {item.title}
+          </a>
+         
+         </li>
+       
+       )
+    
+    })}
+      </ul>
+      <Button>Sign Up</Button>
+      </nav>
+    )
+  }
+   
+  }
+  
+export default Navbar;
+
+}
 
